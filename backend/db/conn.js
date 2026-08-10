184b37ba37a997b739a49be7a727afeddea95161
Evelin <evelin_piva@estudante.sesisenai.org.br>
2026-08-10 20:56:44 -0300

    adicionando banco e alteração no style

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -6,4 +6,12 @@ const db = new Sequelize('db_compras','root','senai',{
     port: 3306
 })
 
+db.authenticate()
+.then(()=>{
+    console.log('conexão realizada com sucesso!')
+})
+.catch((err)=>{
+    console.error('não foi possível conectar com o banco')
+})
+
 module.exports = db
